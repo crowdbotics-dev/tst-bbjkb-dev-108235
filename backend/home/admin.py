088dbc1929487dec3c901f603d615ede5bb0de7d
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-08 15:09:49 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import KKJ
+admin.site.register(KKJ)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class KKJ(models.Model):
+    'Generated Model'
+    sampletest = models.BigIntegerField()
+    print('sampleTest_Run')
